c020eaa57e9c58b0f0310391a5ef89c7751503d3
Kathryn Schuler <kathryn.schuler@gmail.com>
2020-12-29 10:37:00 -0500

    Create tljh_rjulia.py

---
+++ tljh_rjulia.py
@@ -0,0 +1,36 @@
+from tljh.hooks import hookimpl
+from tljh.user import ensure_group
+import sh
+
+@hookspec
+def tljh_extra_user_conda_packages():
+    """
+    Return list of extra conda packages to install in user environment.
+    """
+    return ['r-base', 'r-essentials', 'r-irkernel']
+
+@hookimpl
+def tljh_extra_apt_packages():
+  return ['julia']
+  
+ 
+@hookimpl
+def tljh_config_post_install(config):
+    """
+    Configure /srv/scratch and change configs/mods
+    """
+    ### mkdir -p /srv/scratch
+    ### sudo chown  root:jupyterhub-users /srv/scratch
+    ### sudo chmod 777 /srv/scratch
+    ### sudo chmod g+s /srv/scratch
+    ### sudo ln -s /srv/scratch /etc/skel/scratch
+    # sh.mkdir('/srv/scratch', '-p')
+    # jupyterhub-users doesn't get created until a user logs in
+    # make sure it's created before changing permissions on directory
+    ensure_group('jupyterhub-users') 
+    sh.chown('root:jupyterhub-users', '/opt/tljh/user/lib/R')
+    # sh.chmod('777', '/srv/scratch')
+    # sh.chmod('g+s', '/srv/scratch')
+    # sh.ln('-s', '/srv/scratch', '/etc/skel/scratch')
+
+    
