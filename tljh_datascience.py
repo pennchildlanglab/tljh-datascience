0ec6c716c80bb7ecf759e3968106d151d70903cc
Kathryn Schuler <kathryn.schuler@gmail.com>
2020-12-30 12:24:52 -0500

    Update tljh_datascience.py

--- tljh_datascience.py
+++ tljh_datascience.py
@@ -58,7 +58,7 @@ def tljh_post_install():
         # add the details to use docker spawner with the datascience image
         contents = [
             "c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'",
-            "c.DockerSpawner.image_whitelist = ['jupyter/datascience-notebook:r-4.0.3', 'jupyter/datascience-notebook:r-3.6.6']",
+            "c.DockerSpawner.image_whitelist = ['jupyter/datascience-notebook:r-4.0.3', 'jupyter/datascience-notebook:r-3.6.3']",
             "from jupyter_client.localinterfaces import public_ips",
             "c.JupyterHub.hub_ip = public_ips()[0]",
             "c.DockerSpawner.name_template = '{prefix}-{username}-{servername}'"
